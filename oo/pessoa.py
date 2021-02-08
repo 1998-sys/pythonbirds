539d7982d916999c476ddf27fe47eb3f52015266
1998-sys <matheus.bandeira74@gmail.com>
2021-02-08 10:52:22 -0300

    Criados atributos complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,18 @@
 class Pessoa: #As classes devem começar com letra maiúscula
-    def __init__(self, nome = None,idade=35):
+    def __init__(self, *filhos,nome = None,idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
     def cumprimentar(self): #método está sempre atrelado auma classe
         return f'olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('João')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Matheus'
-    print(p.nome)
-    print(p.idade)
+    renzo = Pessoa(nome='Renzo')
+    Joao = Pessoa(renzo,nome ='João')
+    print(Pessoa.cumprimentar(Joao))
+    print(id(Joao))
+    print(Joao.cumprimentar())
+    print(Joao.nome)
+    print(Joao.idade)
+    for filho in Joao.filhos:
+        print(filho.nome)
