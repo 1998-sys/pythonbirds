6407a060580d68f59dff2558cdf31e0169415235
1998-sys <matheus.bandeira74@gmail.com>
2021-02-08 15:44:48 -0300

    Implementação carro completa

--- oo/Carro.py
+++ oo/Carro.py
@@ -1,34 +1,54 @@
-NORTE='Norte'   #VARIÁVEL CONSTANTE
-SUL= 'Sul'
-LESTE= 'Leste'
-OESTE= 'Oeste'
-
+NORTE = 'Norte'  # VARIÁVEL CONSTANTE
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
 
 
 class Motor:  # definindo a classe motor
     def __init__(self):  # criação do atributo velocidade
         self.velocidade = 0
 
-    def acelerar(self):  #Criação do método acelerar
+    def acelerar(self):  # Criação do método acelerar
         self.velocidade += 1
 
-    def frear(self):     #Criação do método frear
+    def frear(self):  # Criação do método frear
         self.velocidade -= 2
-        self.velocidade = max(0, self.velocidade) # função que controla a velocidade mínima possível 0
+        self.velocidade = max(0, self.velocidade)  # função que controla a velocidade mínima possível 0
 
 
-class Direcao: #Criação da classe direção
- rotação_a_direita_dct = {NORTE:LESTE,LESTE:SUL,SUL:OESTE,OESTE:NORTE} #dictionari
- rotação_a_esquerda_dct ={NORTE:OESTE,LESTE:NORTE,SUL:LESTE,OESTE:SUL}
-    def __init__(self): #criação do atributo valor_de_direcao
-        self.Valor = NORTE #valor inicial começa na direção norte
+class Direcao:  # Criação da classe direção
+    rotação_a_direita_dct = {NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE}  # dictionari
+    rotação_a_esquerda_dct = {NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL}
+
+    def __init__(self):  # criação do atributo valor_de_direcao
+        self.Valor = NORTE  # valor inicial começa na direção norte
+
     def girar_a_direita(self):
-        self.Valor=self.rotação_a_direita_dct[self.Valor]
+        self.Valor = self.rotação_a_direita_dct[self.Valor]
+
     def girar_a_esquerda(self):
-        self.valor=self.rotação_a_esquerda_dct[self.Valor]
+        self.valor = self.rotação_a_esquerda_dct[self.Valor]
 
 
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
 
+    def calcular_velocidade(self):
+        return self.motor.velocidade
 
+    def acelerar(self):
+        self.motor.acelerar()
 
+    def frear(self):
+        self.motor.frear()
 
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,6 +6,12 @@ class Pessoa: #As classes devem começar com letra maiúscula
         self.filhos = list(filhos)
     def cumprimentar(self): #método está sempre atrelado auma classe
         return f'olá {id(self)}'
+    @staticmethod
+    def metodo_estatico():
+        return 42
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
 
 if __name__ == '__main__':
     renzo = Pessoa(nome='Renzo')
@@ -26,3 +32,5 @@ if __name__ == '__main__':
     print(Joao.olhos) #Acessando o atributo olhos através do objeto (João)
     print(renzo.olhos)
     print(id(Pessoa.olhos), id(renzo.olhos), id(Joao.olhos)) #mesmo ID para todos
+    print(Pessoa.metodo_estatico(),Joao.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), Joao.nome_e_atributos_de_classe())
